4acd5e5bf310bb57f1c08a01f236f9a11bb3d077
xfunture <15768184216@163.com>
2024-08-08 18:31:12 +0800

    完成闪电贷的测试工作

--- contracts/UniswapV3FlashLoad.sol
+++ contracts/UniswapV3FlashLoad.sol
@@ -46,6 +46,10 @@ contract UniswapV3FlashLoad{
     // amount0: token0 的借贷数量
     // amount1: token1 的借贷数量
     function flash(uint256 amount0,uint256 amount1) external{
+        console.log("flash function");
+        console.log("msg.sender:",msg.sender);
+        console.log("amount0:",amount0);
+        console.log("amount1:",amount1);
         bytes memory data = abi.encode(
             FlashCallbackData({
                 amount0:amount0,
@@ -65,26 +69,42 @@ contract UniswapV3FlashLoad{
             bytes calldata data
     ) external
     {
-        console.log("uniswapV3FlashCallback function begin\n");
+        console.log("\nuniswapV3FlashCallback function begin");
         console.log("msg.sender:",msg.sender);
+        console.log("token0 address",address(token0));
+        console.log("token1 address",address(token1));
+
+        console.log("fee0:",fee0);
+        console.log("fee1:",fee1);
+        uint256 token0Balance = token0.balanceOf(address(this));
+        console.log("token0 Balance:",token0Balance);
+        uint256 token1Balance = token1.balanceOf(address(this));
+        console.log("token1 Balance:",token1Balance);
         //该函数是回调函数，必须由pool进行调用
         require(msg.sender == address(pool),"not authoried");
         
         //解码数据
         FlashCallbackData memory decoded = abi.decode(data,(FlashCallbackData));
+        console.log("caller:",decoded.caller);
+
         
         // 定制化的代码，一般写的是实现套利步骤
         if(fee0 > 0){
-            // 从个人账户转fee0 数量的token0 到该合约，用于偿还债务
+            // 从个人账户转fee0 数量的token0 到该合约，用于偿还债务利息
             token0.transferFrom(decoded.caller,address(this),fee0);
         }
 
         if(fee1 > 0){
-            // 从个人账户转fee1 数量的token1 到该合约，用于偿还债务
+            // 从个人账户转fee1 数量的token1 到该合约，用于偿还债务利息
+            console.log("\nfee1 > 0:");
+            console.log("decoded.caller:",decoded.caller);
+            //在转账之前，需要先将owner 的weth 代币授权给当前合约，当前合约依然是UniswapV3FlashLoad
+            //但是不能在当前函数调用approve,因为在当前函数调用approve 相当于自己授权给自己，
+            //应当在合约之外进行授权,owner 授权给UniswapV3FlashLoad 合约
             token1.transferFrom(decoded.caller,address(this),fee1);
         }
 
-        // 偿还债务，借款金额加手续费
+        // 偿还债务，借款金额加手利息
         if(fee0 > 0){
             token0.transfer(address(pool),decoded.amount0 + fee0);
         }
@@ -114,6 +134,6 @@ interface IERC20{
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient,uint256 amount) external returns (bool);
     function transferFrom(address sender,address recipient,uint256 amount) external returns (bool);
-    function allownance(address owner,address spender) external view returns (uint256);
+    function allowance(address owner,address spender) external view returns (uint256);
     function approve(address spender,uint256 amount) external returns (bool);
 }
--- test/UniswapV3FlashLoad.ts
+++ test/UniswapV3FlashLoad.ts
@@ -12,14 +12,15 @@ import { FeeAmount } from '@uniswap/v3-sdk';
 
 
 const provider = getProvider();
-const wethContract = new ethers.Contract(WETH_TOKEN.address,ERC20_ABI,provider);
+const wethContract = new ethers.Contract(WETH_TOKEN.address,ERC20_ABI,wallet);
 
 
 
 describe("UniswapV3FlashSwapArbitrate Test",function(){
     async function deployUniswapV3FlashLoadFixture(){
         const [owner,addr1,addr2] = await ethers.getSigners();
-        const uniswapV3FlashLoad = await ethers.deployContract("UniswapV3FlashLoad");
+        const pool = "0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8";
+        const uniswapV3FlashLoad = await ethers.deployContract("UniswapV3FlashLoad",[pool]);
 
         await uniswapV3FlashLoad.waitForDeployment();
 
@@ -29,15 +30,75 @@ describe("UniswapV3FlashSwapArbitrate Test",function(){
         await uniswapv3Swap.waitForDeployment();
 
         console.log("\towner address:",owner.address,"addr1:",addr1.address,"addr2:",addr2.address);
-        console.log("\tuniswapv3SwapFlashSwapArbitrate contract address:",await uniswapV3FlashLoad.getAddress());
+        console.log("\tuniswapV3FlashLoad contract address:",await uniswapV3FlashLoad.getAddress());
         console.log("\tuniswapv3Swap contract address:",await uniswapv3Swap.getAddress());
-        return  {uniswapv3SwapFlashSwapArbitrate: uniswapV3FlashLoad,uniswapv3Swap,owner,addr1,addr2};
+        return  {uniswapV3FlashLoad ,uniswapv3Swap,owner,addr1,addr2};
     }
 
     describe("Deployment",function(){
         it("Should set right owner",async function(){
-            const {uniswapv3SwapFlashSwapArbitrate,uniswapv3Swap,owner,addr1,addr2 } = await loadFixture(deployUniswapV3FlashLoadFixture)
+            const {uniswapV3FlashLoad,uniswapv3Swap,owner,addr1,addr2 } = await loadFixture(deployUniswapV3FlashLoadFixture)
             // expect(await uniswapv3SwapFlashSwapArbitrate.owner()).to.equal(owner.address);
+        })
+    })
+
+    describe("Test Flash load",function(){
+        it("Flash load",async function(){
+            const {uniswapV3FlashLoad,uniswapv3Swap,owner,addr1,addr2 } = await loadFixture(deployUniswapV3FlashLoadFixture)
+            const tokenIn = WETH_TOKEN;
+            const tokenOut = DAI_TOKEN;
+            const inputAmount = 1;
+            const tradeAmount = 0.1;
+            const transferAmount = ethers.parseUnits(inputAmount.toString(),tokenIn.decimals);
+            const amountIn = ethers.parseUnits(tradeAmount.toString(),tokenIn.decimals);
+            const outputAmount = 500;
+            const amountOut = ethers.parseUnits(outputAmount.toString(),tokenOut.decimals);
+            const poolFee = FeeAmount.MEDIUM;
+            const uniswapv3SwapAddress = await uniswapv3Swap.getAddress();
+            const uniswapV3FlashLoadAddress = await uniswapV3FlashLoad.getAddress();
+            let contractwethBalance;
+            // const output = await uniswapv3Swap.swapExactOutputSingle(tokenIn.address,tokenOut.address,0,amountOut);
+
+
+            //------------------------------------------------wrapEther to owner---------------------------------------------------------------------
+            let ownerWethBalance = await wethContract.balanceOf(owner);
+            console.log(`\n\tbefore wrapEther owner weth Balance: ${ethers.formatEther(ownerWethBalance)}`);
+            let tx = await wethContract.deposit({value:transferAmount});
+            await tx.wait();
+            ownerWethBalance = await wethContract.balanceOf(owner);
+            console.log(`\n\tafter wrapEther owner weth Balance: ${ethers.formatEther(ownerWethBalance)}`);
+
+            
+            //-----------------------------------------transfer weth to uniswapV3FlashLoad---------------------------------------------------------------------
+            
+           
+            // const tokenOutContract = new ethers.Contract(tokenOut.address,ERC20_ABI,wallet);
+
+
+            // tx = await wethContract.transfer(uniswapV3FlashLoadAddress,amountIn);
+            // await tx.wait();
+            // contractwethBalance = await wethContract.balanceOf(uniswapV3FlashLoadAddress);
+            // console.log(`\n\tafter transfer weth to uniswapV3FlashLoad contract balance: ${ethers.formatEther(contractwethBalance)}`);
+            
+            //------------------------------------------------owner approve to UniswapV3FLashLoad---------------------------------------------------------------------
+            // 授权是为了在回调函数uniswapV3FlashCallback 中归还fee 数量的借款利息
+            // 归还借款之前需要将owner 中fee数量的代币转给UniswapV3FlashLoad 合约
+            const approveAmount = amountIn;
+            tx = await wethContract.approve(uniswapV3FlashLoadAddress,approveAmount);
+            await tx.wait();
+
+
+            //------------------------------------------------Flash Load---------------------------------------------------------------------
+            const amountToBorrowWETH = ethers.parseUnits('1',WETH_TOKEN.decimals)
+            contractwethBalance = await wethContract.balanceOf(uniswapV3FlashLoadAddress);
+            console.log(`\n\tbefore flash load uniswapV3FlashLoad weth balance: ${ethers.formatEther(contractwethBalance)}`);
+
+            await uniswapV3FlashLoad.flash(0,amountToBorrowWETH);
+            
+            contractwethBalance = await wethContract.balanceOf(uniswapV3FlashLoadAddress);
+            console.log(`\n\tafter flash load uniswapV3FlashLoad weth balance: ${ethers.formatEther(contractwethBalance)}`);
+
+
         })
     })
 
